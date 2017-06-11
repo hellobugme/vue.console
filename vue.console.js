4b1f3983685adee4e93b8b7f0b50ad3978c09f36
hellobugme <1037714455@qq.com>
2017-06-11 18:25:30 +0800

    update

--- vue.console.js
+++ vue.console.js
@@ -1,10 +1,8 @@
-if(Vue){
-    ['log', 'info', 'warn', 'error'].forEach(function(api){
-        var _api = console[api];
-        console[api] = function(){
-            _api.apply(console, [].slice.call(arguments).map(function(arg){
-                return typeof arg === 'object' ? JSON.parse(JSON.stringify(arg)) : arg;
-            }));
-        };
-    });
-}
+;['log', 'info', 'warn', 'error'].forEach(function(api){
+    var _api = console[api];
+    console[api] = function(){
+        _api.apply(console, [].slice.call(arguments).map(function(arg){
+            return typeof arg === 'object' ? JSON.parse(JSON.stringify(arg)) : arg;
+        }));
+    };
+});
